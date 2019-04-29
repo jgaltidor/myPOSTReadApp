96cfcb5fd5183cfbab6a0fcc55977b62b695dce1
John Altidor <john.altidor@cambridgesemantics.com>
2019-04-29 16:18:37 -0400

    Parsing JSON passed in http request

--- src/main/java/jga/postread/App.java
+++ src/main/java/jga/postread/App.java
@@ -5,6 +5,10 @@ import java.util.HashMap;
 import java.util.Map;
 import fi.iki.elonen.NanoHTTPD;
 
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
 public class App extends NanoHTTPD
 {
   public App() throws IOException {
@@ -27,19 +31,34 @@ public class App extends NanoHTTPD
     Map<String, String> files = new HashMap<String, String>();
     Method method = session.getMethod();
     if (Method.PUT.equals(method) || Method.POST.equals(method)) {
-        try {
-            session.parseBody(files);
-        } catch (IOException ioe) {
-            return newFixedLengthResponse(
-              "SERVER INTERNAL ERROR: IOException: " + ioe.getMessage());
-        } catch (ResponseException re) {
-          return newFixedLengthResponse(
-              "SERVER INTERNAL ERROR: ResponseException: " + re.getMessage());
-        }
+      try {
+        session.parseBody(files);
+      }
+      catch (IOException ioe) {
+        return newFixedLengthResponse(
+            "SERVER INTERNAL ERROR: IOException: " + ioe.getMessage());
+      }
+      catch (ResponseException re) {
+        return newFixedLengthResponse(
+            "SERVER INTERNAL ERROR: ResponseException: " + re.getMessage());
+      }
     }
     // get the POST body
     String postBody = files.get("postData");
     System.out.println("postBody: " + postBody);
+    JSONParser parser = new JSONParser();
+    JSONObject json;
+    try {
+      json = (JSONObject) parser.parse(postBody);
+    }
+    catch(ParseException e) {
+      return newFixedLengthResponse(
+          "SERVER INTERNAL ERROR: ParseException: " + e.getMessage());
+    }
+    String username = (String) json.get("username");
+    String password = (String) json.get("password");
+    System.out.println("username: " + username);
+    System.out.println("password: " + password);
     StringBuffer rsb = new StringBuffer("<html><body>")
       .append("<p>postBody: ").append(postBody).append("</p>")
       .append("</body></html>\n");
